e6accfd41a71a364aa51b6deca8610ea785acc06
Rob Clewley <robclewley@Roberts-MacBook-Pro.local>
2019-01-03 13:32:26 -0500

    Create API_lib.py

---
+++ API_lib.py
@@ -0,0 +1,55 @@
+import os, sys
+import json
+import urllib
+
+url_lookup = {'prod': 'https://aping-pong.herokuapp.com',
+              'dev': 'localhost:5000'}
+
+global server_url
+
+def use_server_url(kind):
+    global server_url
+    server_url = url_lookup[kind]
+
+def call(path, id_code='', show_cmd=False, is_post=False, **kwargs):
+    """Execute curl commands to access game API endpoints.
+
+    Inputs
+    ------
+    path:      API endpoint leading slash [string]
+    id_code:   Unique private id [string]
+    show_cmd:  Print the full curl command executed to stdout [bool, default False]
+    is_post:   Not currently used for this game, but controls how parameters are passed
+    **kwargs:  Other keyword arguments to pass either as regular parameters (is_post=False) or
+                 as form parameters (is_post=True)
+    """
+    params = []
+    if is_cli:
+        params.append(('cli', 'true'))
+    if id_code != '':
+        params.append(('private_id', id_code))
+    path = urllib.parse.quote(path)
+    # silent mode for curl
+    cmd = f'curl -s "{server_url}/{path}'
+    if is_post:
+        # add trailing double quote prior to form params
+        cmd += '?' + urllib.parse.urlencode(params) + '"'
+        for k, v in kwargs.items():
+            cmd += f" --form {k}={v}"
+    else:
+        params.extend(list(kwargs.items()))
+        cmd += '?' + urllib.parse.urlencode(params) + '"'
+    if show_cmd:
+        print(cmd)
+    res = os.popen(cmd).read()
+    try:
+        return json.loads(res)
+    except:
+        return res
+
+def declare_player(level, pID=None, show_cmd=False):
+    if pID is None:
+        info = call('request_game/{}'.format(level), show_cmd=show_cmd)
+    else:
+        info = call('request_game/{}'.format(level), id_code=pID, show_cmd=show_cmd)
+    return info
